7c24287b11864f3d2d5823455adfe917b500004c
neelakandan <neelakandan2001kpm@gmail.com>
2024-08-09 13:05:10 +0530

    Packages

---
+++ 05-Importing Modules and Package/package/__init__.py
@@ -0,0 +1,2 @@
+## __init__.py is a special file in python to define packages and initialize their namespace
+
---
+++ 05-Importing Modules and Package/test.py
@@ -0,0 +1,6 @@
+from package.maths import *
+from package.subpackage.mult import *
+
+print(addition(9,8))
+
+print(multiple(2,6))
